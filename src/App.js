0ce1086f5eb36e601cc0bfb595a34ddb3e9d809e
Naman Modani <modani.naman@gmail.com>
2022-07-24 21:40:59 +0530

    Imported Geist UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Page, Text, Image, Display, Button, Grid } from '@geist-ui/react'
 
-function App() {
+const gh = 'https://github.com/geist-org/react'
+const docs = 'https://react.geist-ui.dev'
+
+const App = () => {
+  const redirect = url => window.open(url)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Page dotBackdrop width="800px" padding={0}>
+      <Display
+        title="Geist UI"
+        caption={
+          <>
+            Welcome to{' '}
+            <Text span b>
+              Geist UI
+            </Text>{' '}
+            and start learning more !
+          </>
+        }>
+        <Image src="/geist-banner.png" draggable={false} />
+      </Display>
+      <Grid.Container justify="center" gap={3} mt="100px">
+        <Grid xs={20} sm={7} justify="center">
+          <Button shadow type="secondary-light" onClick={() => redirect(gh)}>
+            GitHub Repo
+          </Button>
+        </Grid>
+        <Grid xs={0} sm={3} />
+        <Grid xs={20} sm={7} justify="center">
+          <Button onClick={() => redirect(docs)}>Documentation Site</Button>
+        </Grid>
+      </Grid.Container>
+    </Page>
+  )
 }
 
-export default App;
+export default App
